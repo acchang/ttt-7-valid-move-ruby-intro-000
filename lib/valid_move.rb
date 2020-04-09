3e5352599e5f89b5531a807bac98f3ef76910f26
Andrew C <acchang4@gmail.com>
2020-04-09 19:19:01 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,10 +7,9 @@ def position_taken?(board, index)
 end
 
   def valid_move?(board, index)
-      if !position_taken?(board, index) == true && index.between?(0, 8)
+      if position_taken?(board, index) == false && index.between?(0, 8) 
           return true
         else
           return false
         end
       end
-end
