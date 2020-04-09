cd2fd5eb1fddc285847075eea49268d41b91ddcf
Andrew C <acchang4@gmail.com>
2020-04-09 17:30:21 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,24 +1,26 @@
-        def valid_move?(board,index)
-          def position_taken?(board,index)
-            if (board[index]) == " " || (board[index]) == "" || (board[index]) == nil
-              false
-            else
-              true
-            end
-          end
 
-          def on_board?(num)
-            if num.between?(0, 8) == true
-              return true
-            else
-              return false
-            end
-          end
+def valid_move?(board, index)
+  def position_taken?(board, index)
+    if (board[index]) == " " || (board[index]) == "" || (board[index]) == nil
+      return false
+    else
+      return true
+    end
+  end
 
-          if (position_taken?(board, index)) == false && (on_board?(index) == true)
-            return true
-          else
-            return false
-          end
-      end
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
+
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
+  else
+    return false
+  end
+
+
 end
