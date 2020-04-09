12cf93da5adcb5b41dce329bd0ea8697cb53b16a
Andrew C <acchang4@gmail.com>
2020-04-09 17:22:51 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,6 +6,15 @@
         end
       end
 
+      def valid_move?(board,index)
+        def position_taken?(board,index)
+          if (board[index]) == " " || (board[index]) == "" || (board[index]) == nil
+            false
+          else
+            true
+          end
+        end
+
       if (position_taken?(board, index)) == false && (on_board?(index) == true)
         return true
       else
