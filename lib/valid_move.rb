9215cc747d01866e6915ce83dcb7126fc98e14f5
Andrew C <acchang4@gmail.com>
2020-04-09 17:32:15 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,21 +6,4 @@
     end
   end
 
-  def valid_move?(board, index)
-    def position_taken?(board, index)
-      if (board[index]) == " " || (board[index]) == "" || (board[index]) == nil
-        return false
-      else
-        return true
-      end
-    end
-
-
-  if (position_taken?(board, index)) == false && (on_board?(index) == true)
-    return true
-  else
-    return false
-  end
-
-
 end
